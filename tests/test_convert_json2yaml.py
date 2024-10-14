9ad76a5dde68ede74aebcdb09abf60fe5c91cada
cu <luqin.li@lkcoffee.com>
2024-10-14 16:52:11 +0800

    update

--- tests/test_convert_json2yaml.py
+++ tests/test_convert_json2yaml.py
@@ -1,5 +1,5 @@
 import json
-from server.convert import json2yaml
+from utils.convert import json2yaml
 
 
 # test
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     with open(json_file_path, "r",  encoding="utf-8") as f:
         json_str = f.read()
 
-    yaml_str = json2yaml(json_str)
+    yaml_str, _ = json2yaml(json_str)
 
     with open(yaml_file_path, "w", encoding="utf-8") as f:
         f.write(yaml_str)
