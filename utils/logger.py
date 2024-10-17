cc195612f8f4697b12fad6e5a93e78a991b95235
cu <luqin.li@lkcoffee.com>
2024-10-17 14:05:21 +0800

    存放到tmp下

--- utils/logger.py
+++ utils/logger.py
@@ -1,42 +1,37 @@
 import os
 import logging
+from logging.handlers import RotatingFileHandler
 
-
-def setup_logger(name, comp_name="DAG", job_id=None):
-    # 创建日志目录（如果不存在）
-    
-    if job_id:
-        log_dir = os.path.join(os.path.dirname(__file__), f'../cache/{job_id}/logs')
-    else:
-        log_dir = os.path.join(os.path.dirname(__file__), '../cache/logs')
-    os.makedirs(log_dir, exist_ok=True)
-
-    # 创建日志文件路径
-    log_file = os.path.join(log_dir, f"{name}.log")
-
-    # 创建日志记录器
-    logger = logging.getLogger(name)
+def setup_logger(name, prefix, job_id=None):
+    logger = logging.getLogger(f"{prefix}_{name}")
     logger.setLevel(logging.INFO)
 
-    # 创建文件处理器
-    file_handler = logging.FileHandler(log_file)
+    # 使用固定的日志目录
+    base_log_dir = '/tmp/lego/log'  # 或者其他您指定的固定路径
+    if job_id:
+        log_dir = os.path.join(base_log_dir, job_id)
+    else:
+        log_dir = base_log_dir
+
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = os.path.join(log_dir, f'{name}.log')
+
+    # 文件处理器
+    file_handler = RotatingFileHandler(log_file, maxBytes=10*1024*1024, backupCount=5)
     file_handler.setLevel(logging.INFO)
 
-    # 创建控制台处理器
+    # 控制台处理器
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    # 创建格式化器
-    formatter = logging.Formatter(f"[{comp_name}]: %(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                                  datefmt="%Y-%m-%d %H:%M:%S")
+    # 格式化器
+    formatter = logging.Formatter(f'[{prefix}]: %(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
 
-    # 将格式化器添加到处理器
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
-    # 将处理器添加到日志记录器
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     return logger
-
