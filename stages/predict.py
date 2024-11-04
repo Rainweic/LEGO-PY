244ca1cbb6381e949a5c1b3f3f9b73db0c67e1f2
cu <luqin.li@lkcoffee.com>
2024-11-04 16:22:45 +0800

    新增阈值

--- stages/predict.py
+++ stages/predict.py
@@ -5,12 +5,25 @@ from stages.xgb import XGB
 
 class Predict(CustomStage):
 
-    def __init__(self):
+    def __init__(self, threshold=0.5):
         super().__init__(n_outputs=1)
+        self.threshold = threshold
 
     def forward(self, model, lf: pl.LazyFrame):
+
+        if self.threshold > 1 or self.threshold < 0:
+            self.logger.error("阈值的取值范围应该是(0, 1)")
+            raise TypeError("阈值的取值范围应该是(0, 1)")
 
         model_type = model['type']
         
         if model_type == "XGB":
-            return XGB.predict(model=model, lf=lf)
+            result = XGB.predict(model=model, lf=lf)
+
+        if "y_score" in result.columns:
+            result = result.with_columns(
+                pl.col("*"),
+                pl.when(pl.col("y_score") > self.threshold).then(1).otherwise(0).alias("y_pred")
+            )
+        
+        return result
